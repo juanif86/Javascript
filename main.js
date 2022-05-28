4167fd8f71a7a0b48b5f30a461bfd91ccca66096
JuaniF <juanignaciof86@gmail.com>
2022-05-28 13:00:50 -0300

    defino variables y cambio parametros

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+//Aplicación que calcula la potencia de un número
 console.log("Simulador que calcula la potencia de un número")
-
+//Pido mediante un prompt que ingrese datos
 base = prompt("ingrese el numero que desea elevar: ")
 exponente = prompt("ingrese el exponente: ")
-
+//defino variables
 resultado=1
+//creo una función que utiliza los datos ingresados y devuelve como resultado
+//la potencia del numero ingresado
+//la función utiliza un ciclo para iterar
 function potencia(base,exponente){
     for(i=1;i<=exponente;i++){
         resultado = base*resultado
     }
     return resultado;
 }
-
+//llamo a la función
 potencia(base,exponente)
-
+//muestro el resultado en consola
 console.log(resultado)
